c9c0223b1d3e71161b5b811ec432b563c2121213
Lucas G. Alonso <56822984+lgalonso@users.noreply.github.com>
2022-05-20 14:15:56 +0200

    updated output

--- swg.py
+++ swg.py
@@ -29,9 +29,9 @@ def set_target_file_urls(target_file):
         urls = file.read().splitlines()
     
 def get_words_from_url(url, n):
-    os.system("printf '\n\n'")
+    os.system("printf '\nUsing: '")
     os.system("sudo cewl -d 2 -m 8 -w " + n + "_" + target + "_temp.txt " + url)
-    os.system("printf '\n\n\n'")
+    os.system("printf '\n\n'")
 
 def create_temp_files():
     for index, url in enumerate(urls):
